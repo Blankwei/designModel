2232efaf7c758ff0d4a32fe6b29241a3ed9dbe77
savoidage <592232644@qq.com>
2020-04-30 10:45:07 +0800

    Initial commit

---
+++ factory_mothod/src/main/java/com/savoidage/design/factory_mothod/FactoryMothodApplication.java
@@ -0,0 +1,14 @@
+package com.savoidage.design.factory_mothod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryMothodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactoryMothodApplication.class, args);
+        System.out.println("factory_method 启动成功...");
+    }
+
+}
